910057b8cbf42707f7c7ffb19f2bc7408d0a57f8
Ou <orongxing@gmail.com>
2024-09-06 15:23:19 +0800

    chore: update library list

--- scripts/genDoc.ts
+++ scripts/genDoc.ts
@@ -4,15 +4,17 @@ import fs from "fs-extra"
 import type { Target } from "@/types"
 
 const fileName = "词库.md"
-function genDownloadURL(name: string, target: Target) {
+function genTableItem(name: string, target: Target) {
   const suffix = target === "translation" ? "csv" : "txt"
-  return `[⬇️](https://github.com/busiyiworld/maimemo-export/blob/main/exported/${target}/${encodeURIComponent(name)}.${suffix})`
+  const encodeName = encodeURIComponent(name)
+  return `<a href="https://github.com/busiyiworld/maimemo-export/blob/main/exported/${target}/${encodeName}.${suffix}">📖</a> <a href="https://raw.githubusercontent.com/busiyiworld/maimemo-export/main/exported/${target}/${encodeName}.${suffix}" download>⬇️</a>`
 }
+
 async function main() {
   const libs = getLibs("base")
   const content = markdownTable([
     ["词库名", "仅单词", "单词和章节", "单词和翻译"],
-    ...libs.map(k => [k.name, genDownloadURL(k.name, "word"), genDownloadURL(k.name, "list"), genDownloadURL(k.name, "translation")]),
+    ...libs.map(k => [k.name, genTableItem(k.name, "word"), genTableItem(k.name, "list"), genTableItem(k.name, "translation")]),
   ], { align: ["l", "c", "c", "c"] })
   await fs.writeFile(fileName, content)
 }
